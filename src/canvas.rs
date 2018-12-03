35bc6852f75e7c037c0c499b0acbb4e4cbf83570
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2018-12-03 17:00:51 +0200

    use canvas actor instead of shared state

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,3 +1,5 @@
+use actix::prelude::*;
+
 pub type Color = u8;
 
 #[derive(Debug)]
@@ -7,27 +9,68 @@ pub struct Canvas {
   data: Vec<Color>,
 }
 
+impl Actor for Canvas {
+  type Context = Context<Self>;
+}
+
 impl Canvas {
-  pub fn new(width: usize, height: usize, data: Vec<Color>) -> Self {
-    assert!(data.len() == width * height);
-    Self { width, height, data }
+  pub fn new(width: usize, height: usize) -> Self {
+    Self { width, height, data: vec![0; width * height] }
   }
 
-  pub fn get(&self, x: usize, y: usize) -> Color {
-    self.assert_in_bounds(x, y);
-    self.data[y * self.width + x]
-  }
-
-  pub fn set(&mut self, x: usize, y: usize, color: Color) {
-    self.assert_in_bounds(x, y);
-    self.data[y * self.width + x] = color;
-  }
-
-  fn assert_in_bounds(&self, x: usize, y: usize) {
+  fn assert_in_bounds(&self, x: usize, y: usize) -> Option<String> {
     let w = self.width;
     let h = self.height;
 
-    assert!(x < w, "x out of bounds: the width is {} but the x is {}", w, x);
-    assert!(y < h, "y out of bounds: the height is {} but the y is {}", h, y);
+    macro_rules! err {
+      ($cond:expr, $($arg:tt)+) => {
+        if $cond { return Some(format!($($arg)+)); }
+      };
+    }
+
+    err!(x >= w, "x out of bounds: the width is {} but the x is {}", w, x);
+    err!(y >= h, "y out of bounds: the height is {} but the y is {}", h, y);
+
+    None
+  }
+}
+
+#[derive(Debug, Deserialize, Message)]
+#[rtype("Result<u8, String>")]
+pub struct GetCell {
+  pub x: usize,
+  pub y: usize,
+}
+
+impl Handler<GetCell> for Canvas {
+  type Result = <GetCell as Message>::Result;
+
+  fn handle(&mut self, msg: GetCell, _: &mut Self::Context) -> Self::Result {
+    if let Some(err) = self.assert_in_bounds(msg.x, msg.y) {
+      return Err(err);
+    }
+
+    Ok(self.data[msg.y * self.width + msg.x])
+  }
+}
+
+#[derive(Debug, Deserialize, Message)]
+#[rtype("Result<(), String>")]
+pub struct UpdateCell {
+  pub x: usize,
+  pub y: usize,
+  pub color: u8,
+}
+
+impl Handler<UpdateCell> for Canvas {
+  type Result = <UpdateCell as Message>::Result;
+
+  fn handle(&mut self, msg: UpdateCell, _: &mut Self::Context) -> Self::Result {
+    if let Some(err) = self.assert_in_bounds(msg.x, msg.y) {
+      return Err(err);
+    }
+
+    self.data[msg.y * self.width + msg.x] = msg.color;
+    Ok(())
   }
 }
