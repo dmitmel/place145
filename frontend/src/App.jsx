e505f46ff69afa4b1ca027f179a8ea2a840cfea6
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2018-12-20 09:27:54 +0200

    fetch canvas before rendering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { width, height } from './config';
 import CanvasRenderer from './CanvasRenderer';
 import './App.scss';
 
-export default function App() {
-  return <CanvasRenderer />;
+export default class App extends React.Component {
+  canvasRef = React.createRef();
+
+  componentDidMount() {
+    this.fetchEntireCanvas();
+  }
+
+  fetchEntireCanvas() {
+    fetch('/api/canvas')
+      .then(response => response.arrayBuffer())
+      .then(arrayBuffer => {
+        const data = Buffer.from(arrayBuffer);
+        data.forEach((color, index) => {
+          const y = Math.floor(index / height);
+          const x = index % width;
+          this.canvasRef.current.setCell(x, y, color);
+        });
+      })
+      .catch(console.error);
+  }
+
+  render() {
+    return <CanvasRenderer ref={this.canvasRef} />;
+  }
 }
